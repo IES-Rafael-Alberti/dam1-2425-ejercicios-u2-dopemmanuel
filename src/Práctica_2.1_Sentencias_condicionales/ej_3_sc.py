5170aa47ecbadc4c80b70479797b2ce37fc98d63
dopemmanuel <dopemmanuel@iesrafaelalberti.es>
2024-10-19 00:10:07 +0200

    ( •_•)>⌐■-■

--- src/Práctica_2.1_Sentencias_condicionales/ej_3_sc.py
+++ src/Práctica_2.1_Sentencias_condicionales/ej_3_sc.py
@@ -2,39 +2,33 @@
 muestre por pantalla su división. Si el divisor es cero 
 el programa debe mostrar un error."""
 
-
 def numero_divisores(num1, num2):
-    """Condiciones"""
 
-    if num1 == 0 or num2 == 0:
+    """Condiciones para comprobar y evitar que las variables num1 y num2 sean igual a 0
+    afirmandolos con true y false.
+    """
+
+    if num1 == 0:
+        print("Pana, hay un pequeño error aqui, el primer numero no puede ser cero.")
+        return False
+    elif num2 == 0:
+        print("Eeo Compadre, es imposible dividir entre cero")
         return False
     else:
         division = num1 / num2
         print(f"{num1} entre {num2} da {round(division)}")
         return True
-    
-def confirmacion(confirmar):
-    
-    """Hola"""
-    if confirmar is True:
-        return True
-    else:
-        return False
-
 
 def main():
 
-    """Variables y condiciones"""
+    """Variables num1 y num2 que son tipo int y que va a almacenar los numeros a dividir
+    """
     num1 = int(input("Ingrese el primer numero → "))
     num2 = int(input("Ingrese el segundo numero → "))
 
-
-    confirmar = numero_divisores(num1, num2)
-
-    while confirmacion(confirmar) == numero_divisores(num1, num2):
-
-        print("Eeo Compadre, es imposible dividir entre cero")
-        num1 = int(input("Ingrese el primer numero → "))
+    while not numero_divisores(num1, num2):
+        num1 = int(input("Ingrese el primer numero que no sea 0 → "))
+        num2 = int(input("Ingrese un segundo numero que no sea 0 → "))
 
 if __name__== "__main__":
     main()
