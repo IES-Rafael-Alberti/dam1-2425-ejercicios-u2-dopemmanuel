3fdfa944b5337172a8b5d1379625a79f680fb400
dopemmanuel <dopemmanuel@iesrafaelalberti.es>
2024-10-25 04:03:57 +0200

    😭👌

--- src/Práctica_2_3_Capturas_de_excepciones/ej23_03.py
+++ src/Práctica_2_3_Capturas_de_excepciones/ej23_03.py
@@ -3,3 +3,46 @@ Escribir un programa que pida al usuario un número entero positivo
 y muestre por pantalla la cuenta atrás desde ese número hasta cero separados por comas. 
 Deberá solicitar el número hasta introducir uno correcto.
 """
+
+def numero_ntero(numr):
+
+    """OK"""    
+    entero = ""
+    for inicial in range(int(numr), 0 ,-1):
+        entero += str(inicial) + ","
+
+    return entero[:-1]
+
+def main():
+
+    """OKS"""
+    no_letra = True
+    salida_ra = True
+    while salida_ra:
+        try:
+            numr = input("Ingresa un numero entero → ")
+
+            no_letra = False
+            if "." in numr:
+                raise ValueError("Nada de decimales (￣︿￣*()")
+
+            no_letra = True
+            numr = int(numr)
+            no_letra = False
+
+            print(numero_ntero(numr))
+
+            #askers = input("Quieres salir? (s/n) → ")
+            #if askers != "s":
+                #salida_ra = False
+
+            salida_ra = False
+
+        except ValueError as e:
+            if no_letra:
+                print("Tampoco se aceptan letras...")
+            else:
+                print(f"{e} ")
+
+if __name__ == "__main__":
+    main()
--- src/Práctica_2_3_Capturas_de_excepciones/ej23_04.py
+++ src/Práctica_2_3_Capturas_de_excepciones/ej23_04.py
@@ -2,3 +2,44 @@
 Escribir un programa que pida al usuario un número entero, si la entrada no es correcta, 
 mostrará el mensaje "La entrada no es correcta" y lanzará la excepción capturada.
 """
+
+def captura_exepciones():
+    """
+    Esta funcion contiene variables, dos condiciones una dentro de otra
+    y exeptiones:
+    """
+    noletras = True
+    salid = True
+    while salid:
+        try:
+            nums = input("Ingresa el numero aqui → ")
+            noletras = False
+
+            if "." in nums:
+                raise ValueError("La entrada no es correcta.")
+
+            noletras = True
+            nums = int(nums)
+            noletras = False
+
+            #salida = input("¿Reintentar? (s/n) → ")
+            #if salida != "n":
+                #salid = False
+
+            salid = False
+
+        except ValueError as e:
+            if noletras:
+                print("Nada de letras, solo numeros.")
+            else:
+                print(f"{e}")
+
+    return nums
+
+def main():
+    """La funcion main solo imprimira el resultado"""
+
+    print(f"Esta correcto, {captura_exepciones()} es el numero entero.")
+
+if __name__ == "__main__":
+    main()
